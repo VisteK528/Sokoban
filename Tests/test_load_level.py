7c82015bfddfc7d9a521496d14a7fefbd33e1417
Piotr Patek <piotr.patek.stud@pw.edu.pl>
2022-12-22 19:07:13 +0100

    Updated tests for checking_requirements

--- Tests/test_load_level.py
+++ Tests/test_load_level.py
@@ -1,29 +1,69 @@
 from load_level import (
     LoadLevel,
-    InvalidTileSizeError,
+    check_requirements,
+    TooManyPlayersFoundError,
     InvalidDimensionsError,
+    NoPlayerFoundError,
+    UnmachtingBoxCountError
     )
 from pytest import raises
 
 
 def test_save_data_to_json():
-    data = {0: {0: 1, 1: 0, 2: 3},
-            1: {0: 1, 1: 0, 2: 4},
-            2: {0: 1, 1: 0, 2: 2}}
-    with open("Tests/test_save_level.json", "w") as fp:
-        LoadLevel.write_to_json(fp, data)
+    data = {"0": {"0": 1, "1": 0, "2": 3},
+            "1": {"0": 1, "1": 0, "2": 5},
+            "2": {"0": 1, "1": 0, "2": 2}}
+    path = "Tests/test_save_level.json"
+    load_level = LoadLevel()
+    load_level.save_to_file(path, data)
 
 
-def test_loadlevel_negative_dimensions():
+def test_check_requirements_with_no_player():
+    data = {"0": {"0": 1, "1": 0, "2": 3},
+            "1": {"0": 1, "1": 0, "2": 0},
+            "2": {"0": 1, "1": 0, "2": 2}}
+    rows = 3
+    columns = 3
+    with raises(NoPlayerFoundError):
+        check_requirements(rows, columns, data)
+
+
+def test_check_requirements_with_unmatching_number_of_boxes():
+    data = {"0": {"0": 1, "1": 0, "2": 3},
+            "1": {"0": 1, "1": 3, "2": 5},
+            "2": {"0": 1, "1": 0, "2": 2}}
+    rows = 3
+    columns = 3
+    with raises(UnmachtingBoxCountError):
+        check_requirements(rows, columns, data)
+
+
+def test_check_requirements_with_too_many_players():
+    data = {"0": {"0": 1, "1": 0, "2": 3},
+            "1": {"0": 5, "1": 0, "2": 5},
+            "2": {"0": 1, "1": 0, "2": 2}}
+    rows = 3
+    columns = 3
+    with raises(TooManyPlayersFoundError):
+        check_requirements(rows, columns, data)
+
+
+def test_check_requirements_with_unmatching_number_of_rows():
+    data = {"0": {"0": 1, "1": 0, "2": 3},
+            "1": {"0": 5, "1": 0, "2": 5},
+            "2": {"0": 1, "1": 0, "2": 2},
+            "3": {"0": 1, "1": 0, "2": 2}}
+    rows = 3
+    columns = 3
     with raises(InvalidDimensionsError):
-        LoadLevel(-50, 1000, 50)
+        check_requirements(rows, columns, data)
 
 
-def test_loadlevel_with_negative_tile_size():
-    with raises(InvalidTileSizeError):
-        LoadLevel(1000, 1000, 0)
-
-
-def test_loadlevel_with_invalid_dimensions():
+def test_check_requirements_with_unmatching_number_of_columns():
+    data = {"0": {"0": 1, "1": 0, "2": 3, "3": 0},
+            "1": {"0": 5, "1": 0, "2": 5, "3": 0},
+            "2": {"0": 1, "1": 0, "2": 2, "3": 0}}
+    rows = 3
+    columns = 3
     with raises(InvalidDimensionsError):
-        LoadLevel(1001, 1001, 50)
+        check_requirements(rows, columns, data)
