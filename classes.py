373f7491fd31ccaf218fd286c8c2676274dc4b04
Piotr Patek <piotr.patek.stud@pw.edu.pl>
2022-12-25 20:44:38 +0100

    Created common class Entity for all game objects

---
+++ classes.py
@@ -0,0 +1,53 @@
+import pygame
+
+
+class Entity(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.position = pygame.math.Vector2(x, y)
+
+    def update_visual(self, tile_size):
+        self.rect.x = self.position.x * tile_size
+        self.rect.y = self.position.y * tile_size
+
+
+class Player(Entity):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.image = pygame.image.load("Textures/Player.png")
+        self.rect = self.image.get_rect(topleft=(0, 0))
+
+        self.direction = pygame.math.Vector2(0, 0)
+        self.clicked = False
+
+        self.moves = 0
+        self.pushes = 0
+
+
+class Tile(Entity):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.image = pygame.image.load("Textures/Tile.png")
+        self.rect = self.image.get_rect(topleft=(0, 0))
+
+
+class Box(Entity):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self._default_image = pygame.image.load("Textures/Box.png")
+        self._change_image = pygame.image.load("Textures/Box_2.png")
+        self.set_default_image()
+        self.rect = self.image.get_rect(topleft=(0, 0))
+
+    def set_default_image(self):
+        self.image = self._default_image
+
+    def set_change_image(self):
+        self.image = self._change_image
+
+
+class BoxTarget(Entity):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.image = pygame.image.load("Textures/Box_target.png")
+        self.rect = self.image.get_rect(topleft=(0, 0))
