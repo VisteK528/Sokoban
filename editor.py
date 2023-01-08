3508f710ae98ff0d82b28c8170e25139363f9fbe
Piotr Patek <piotr.patek.stud@pw.edu.pl>
2023-01-08 20:06:11 +0100

    Fixed typo

--- editor.py
+++ editor.py
@@ -2,5 +2,5 @@ from level_editor import LevelEditor
 from settings import level_width, level_height
 
 if __name__ == "__main__":
-    editor = LevelEditor(level_height, level_width, "Levels/")
+    editor = LevelEditor(level_width, level_height, "Levels/")
     editor.run()
